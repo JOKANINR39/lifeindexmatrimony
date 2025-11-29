a66201beb1061b4a6abad05235e6035f5cbbd877
JOKANINR39 <23BCS039@PSGRKCW.AC.IN>
2025-11-29 13:09:02 +0530

    Update print statement from 'Hello' to 'Goodbye'

--- home.php
+++ home.php
@@ -3,7 +3,7 @@ session_start();
 include("db.php");
 
 if (!isset($_SESSION['user_id'])) {
-  header("Location: login.php");
+  header("Location: index.html");
   exit();
 }
 
@@ -625,4 +625,5 @@ include("header.php");
   </script>
 </body>
 </html>
+
 <?php include("footer.php"); ?>
